add96d522b8e339ace6e1edbc701d723fd3d5509
Borzoi <siteandstock@gmail.com>
2025-01-12 16:41:37 +0100

    [4] rename and simplify largerNumber fun, remove semicolons as being unnecessary

--- app/src/main/java/com/example/helloworld3/LearnKotlin.kt
+++ app/src/main/java/com/example/helloworld3/LearnKotlin.kt
@@ -2,17 +2,17 @@ package com.example.helloworld3
 
 import kotlin.math.max
 
+
 fun main() {
     var a: Int = 10
     a *= 10
     println("a = $a")
     println("methodName fun returns: " + methodName(1,2))
-    println("largeNum fun returns: " + largeNum(13,5))
+    println("largerNumber fun returns: " + largerNumber(13,5))
 }
 
 fun methodName(param1: Int, param2: Int): Int {
-    return 0;
+    return 0
 }
-fun largeNum(param1: Int, param2: Int): Int {
-    return max(param1, param2);
-}
+
+fun largerNumber(param1: Int, param2: Int) = max(param1, param2)
