a40f40d4068bffba86614ec19343c4c07d81dd36
Borzoi <siteandstock@gmail.com>
2025-01-12 17:23:33 +0100

    [8] simplify the largerNumber fun even further

--- app/src/main/java/com/example/helloworld3/LearnKotlin.kt
+++ app/src/main/java/com/example/helloworld3/LearnKotlin.kt
@@ -4,18 +4,16 @@ fun main() {
     var a: Int = 10
     a *= 10
     println("a = $a")
-    println("methodName fun returns: " + methodName(1,2))
-    println("largerNumber fun returns: " + largerNumber(13,5))
+    println("methodName fun returns: " + methodName(1, 2))
+    println("largerNumber fun returns: " + largerNumber(13, 5))
 }
 
 fun methodName(param1: Int, param2: Int): Int {
     return 0
 }
 
-fun largerNumber(param1: Int, param2: Int): Int {
-    return if (param1 > param2) {
-        param1
-    } else {
-        param2
-    }
+fun largerNumber(param1: Int, param2: Int) = if (param1 > param2) {
+    param1
+} else {
+    param2
 }
