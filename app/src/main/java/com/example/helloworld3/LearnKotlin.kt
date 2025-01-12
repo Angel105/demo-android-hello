3e7f51c64c436b88fa84467e34b64d1e301741ec
Borzoi <siteandstock@gmail.com>
2025-01-12 16:20:03 +0100

    [3] create and make use of two more funs

--- app/src/main/java/com/example/helloworld3/LearnKotlin.kt
+++ app/src/main/java/com/example/helloworld3/LearnKotlin.kt
@@ -1,6 +1,18 @@
 package com.example.helloworld3
 
+import kotlin.math.max
+
 fun main() {
-    val a = 10
+    var a: Int = 10
+    a *= 10
     println("a = $a")
+    println("methodName fun returns: " + methodName(1,2))
+    println("largeNum fun returns: " + largeNum(13,5))
+}
+
+fun methodName(param1: Int, param2: Int): Int {
+    return 0;
+}
+fun largeNum(param1: Int, param2: Int): Int {
+    return max(param1, param2);
 }
