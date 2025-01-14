4962b54ba3991e756fde650e046ae894cae97cb5
Borzoi <siteandstock@gmail.com>
2025-01-14 18:24:30 +0100

    [26] declare a secondary constructor with the keyword constructor, and define here two secondary constructors: the first secondary constructor takes name and age parameter, and then it calls primary constructor by using this keyword and initializes sno and grade; the second secondary constructor does not have any parameter and it uses this keyword to call the first secondary constructor we just defined and initializes name and age.

--- app/src/main/java/com/example/helloworld3/Student.kt
+++ app/src/main/java/com/example/helloworld3/Student.kt
@@ -1,8 +1,9 @@
 package com.example.helloworld3
 
 class Student(val sno: String, val grade: Int, name: String, age:Int): Person(name, age) {
-    init {
-        println("sno is $sno")
-        println("grade is $grade")
+    constructor(name:String, age:Int) : this("", 0, name, age) {
+    }
+
+    constructor(): this("", 0) {
     }
 }
