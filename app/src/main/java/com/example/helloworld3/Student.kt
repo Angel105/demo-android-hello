519d26a18b475c228225ec266233fc3b1dd6f411
Borzoi <siteandstock@gmail.com>
2025-01-14 15:46:10 +0100

    [24] use an init block to add logic such as simply printing out the student number and the value of the grade

--- app/src/main/java/com/example/helloworld3/Student.kt
+++ app/src/main/java/com/example/helloworld3/Student.kt
@@ -1,4 +1,8 @@
 package com.example.helloworld3
 
 class Student(val sno: String, val grade: Int): Person() {
+    init {
+        println("sno is $sno")
+        println("grade is $grade")
+    }
 }
