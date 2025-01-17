53c731518b1b9faaa151166b8c8b176303a36d86
Borzoi <siteandstock@gmail.com>
2025-01-17 19:13:11 +0100

    [31] create a default implementation of a method. If the method has no implementation, the default implementation will be used.

--- app/src/main/java/com/example/helloworld3/Student.kt
+++ app/src/main/java/com/example/helloworld3/Student.kt
@@ -4,8 +4,4 @@ class Student(name:String, age:Int): Person(name, age), Study {
     override fun read() {
         println("$name is reading.")
     }
-
-    override fun doHomework() {
-        println("$name is doing homework.")
-    }
 }
--- app/src/main/java/com/example/helloworld3/Study.kt
+++ app/src/main/java/com/example/helloworld3/Study.kt
@@ -2,5 +2,7 @@ package com.example.helloworld3
 
 interface Study {
     fun read()
-    fun doHomework()
+    fun doHomework() {
+        println("do homework default implementation.")
+    }
 }
