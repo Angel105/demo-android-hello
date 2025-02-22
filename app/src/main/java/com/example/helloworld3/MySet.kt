09248a7f42adeecc013611510aaab657f40378b3
Borzoi <siteandstock@gmail.com>
2025-02-22 15:59:31 +0100

    [86]  define MySet class  that implement Set interface. The constructor of MySet takes a param of type HashSet which works as assisting object. In the implementation of Set interface methods, the corresponding method of HashSet gets called. This essentially is a way of delegation.

---
+++ app/src/main/java/com/example/helloworld3/MySet.kt
@@ -0,0 +1,16 @@
+package com.example.helloworld3
+
+class MySet<T> (private val helperSet: HashSet<T>): Set<T> {
+    override val size: Int
+        get() = helperSet.size
+
+    override fun isEmpty() = helperSet.isEmpty()
+
+    override fun iterator() = helperSet.iterator()
+
+    override fun containsAll(elements: Collection<T>) = helperSet.containsAll(elements)
+
+    override fun contains(element: T) = helperSet.contains(element)
+
+    fun helloWorld() = println("Hello World")
+}
