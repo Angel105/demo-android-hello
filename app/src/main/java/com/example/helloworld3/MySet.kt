10ffa50e0816bf0fd7efce027264241b20c0f008
Borzoi <siteandstock@gmail.com>
2025-02-22 16:08:40 +0100

    [87] redefine MySet class in a Kotlinish way (class deligation): what if there are too many methods in the interface?  It will be very painful to call assistant object’s method one by one. In Java there is no natively supported solution, but in Kotlin class delegation can help resolve this problem. Use "by" keyword for delegation in Kotlin:  add "by" keyword after the interface declaration, followed by the assistant object to eliminate the boilerplate code. This code works exactly as the previous example but is more succinct with the help of class delegation. If we need to override the method ( e.g. isEmpty() ), then we just need to override that specific method, and all other methods will have the implementation of the assistant object by default.

--- app/src/main/java/com/example/helloworld3/MySet.kt
+++ app/src/main/java/com/example/helloworld3/MySet.kt
@@ -1,16 +1,8 @@
 package com.example.helloworld3
 
-class MySet<T> (private val helperSet: HashSet<T>): Set<T> {
-    override val size: Int
-        get() = helperSet.size
-
-    override fun isEmpty() = helperSet.isEmpty()
-
-    override fun iterator() = helperSet.iterator()
-
-    override fun containsAll(elements: Collection<T>) = helperSet.containsAll(elements)
-
-    override fun contains(element: T) = helperSet.contains(element)
+class MySet<T> (private val helperSet: HashSet<T>): Set<T> by helperSet {
 
     fun helloWorld() = println("Hello World")
+
+    override fun isEmpty() = false
 }
